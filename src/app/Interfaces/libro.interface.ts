8154b2af8e743a696fd722774ff9d3691838d493
MoisesHC <genesis2204gamerhc@gmail.com>
2024-06-20 01:09:59 -0600

    Integración de las funciones en el service para los libros

---
+++ src/app/Interfaces/libro.interface.ts
@@ -0,0 +1,7 @@
+export interface LibroInterface{
+    LibreriaMaterialId?: string;
+    Titulo?: string;
+    FechaPublicacion?: Date;
+    Precio?: Float64Array;
+    AutorLibro?: string;
+}
--- src/app/Services/funciones.service.ts
+++ src/app/Services/funciones.service.ts
@@ -2,28 +2,46 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AutorInterface } from '../Interfaces/autor.interface';
+import { LibroInterface } from '../Interfaces/libro.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FuncionesService {
 
-  API: string = 'https://localhost:7197';
+  API_Autor: string = 'https://localhost:7197';
+  API_Libro: string = 'https://localhost:7262';
 
   constructor(private httpClient: HttpClient) { }
 
+
+  // Funciones para Autor
    crear(data: AutorInterface){
-      return this.httpClient.post(this.API + "/Autor/Crear", data);
+      return this.httpClient.post(this.API_Autor + "/Autor/Crear", data);
    }
-
-
    
    GetAutor(): Observable<any>{
-      return this.httpClient.get(this.API + "/Autor/GetAutores").pipe(res => res);
+      return this.httpClient.get(this.API_Autor + "/Autor/GetAutores").pipe(res => res);
    }
 
    GetAutorLibro(autorLibroGuid: string): Observable<any>{
-      return this.httpClient.get(this.API + "/Autor/GetAutorLibro?id=" + autorLibroGuid);
+      return this.httpClient.get(this.API_Autor + "/Autor/GetAutorLibro?id=" + autorLibroGuid);
+   }
+
+   
+
+
+   // Funciones para Libro
+   crearLibro(data: LibroInterface){
+      return this.httpClient.post(this.API_Libro + '/Libro/Crear', data);
+   }
+
+   GetLibro(): Observable<any>{
+      return this.httpClient.get(this.API_Libro + '/Libro/Getlibros').pipe(res => res);
+   }
+
+   GetLibros(LibreriaMaterialId: string): Observable<any>{
+      return this.httpClient.get(this.API_Libro + "/Autor/GetLibro?id=" + LibreriaMaterialId);
    }
 
 }
